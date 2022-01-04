02a299ef1dac8ef69e9cd039c0dd9c80f1e2c751
Vinit Suryarao <suryarao.vinit@hotmail.com>
2022-01-04 14:28:52 +0530

    Add files via upload

---
+++ Init_Only.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace InitOnly
+{
+    class Client
+    {
+        public int ID { get; init; } // Setter define as Init
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client obj = new Client { ID = 1 }; // Set value only when object creation
+            
+            obj.Name = "ABC";
+            obj.City = "Mumbai";
+            Console.WriteLine(obj.ID.ToString()+" "+obj.Name+" "+obj.City);
+
+            obj.Name = "XYZ";
+            obj.City = "Pune";
+            Console.WriteLine(obj.ID.ToString() + " " + obj.Name + " " + obj.City);
+            
+            Console.ReadLine();
+        }
+    }
+}
